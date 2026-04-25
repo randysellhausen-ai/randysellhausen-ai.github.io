a81462c36aa52d6d914801ce0e05cce3194e2c03
randysellhausen-ai <randy.sellhausen@gmail.com>
2026-04-25 03:02:20 -0500

    Update apexsim-debug-control.js

--- apexsim/apexsim-debug-control.js
+++ apexsim/apexsim-debug-control.js
@@ -1,1 +1,13 @@
+// Control-layer wrapper for APEXSIM.Debug
+// Adds setDebugVisible used by the control panel.
 
+window.APEXSIM = window.APEXSIM || {};
+APEXSIM.Debug = APEXSIM.Debug || {};
+
+APEXSIM.Debug._visible = false;
+
+APEXSIM.Debug.setDebugVisible = function (visible) {
+    this._visible = !!visible;
+};
+
+// You can check APEXSIM.Debug._visible inside your existing debug rendering logic.
