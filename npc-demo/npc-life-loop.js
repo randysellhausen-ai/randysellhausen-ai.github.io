7c24b31788a89ee273211f0c6e4730acaf415ff2
randysellhausen-ai <randy.sellhausen@gmail.com>
2026-04-28 03:14:57 -0500

    Update npc-life-loop.js

--- npc-demo/npc-life-loop.js
+++ npc-demo/npc-life-loop.js
@@ -1,1 +1,55 @@
+class NPC {
+  constructor(name) {
+    this.name = name;
+    this.hunger = 100;
+    this.energy = 100;
+    this.mood = 100;
+    this.action = "Idle";
+  }
 
+  updateNeeds() {
+    this.hunger -= 2;
+    this.energy -= 1;
+    this.mood = Math.round((this.hunger + this.energy) / 2);
+  }
+
+  chooseAction() {
+    if (this.hunger < 40) this.action = "Eating";
+    else if (this.energy < 40) this.action = "Resting";
+    else this.action = "Wandering";
+  }
+
+  tick() {
+    this.updateNeeds();
+    this.chooseAction();
+  }
+}
+
+const npcs = [
+  new NPC("Ava"),
+  new NPC("Jace"),
+  new NPC("Mira"),
+];
+
+function render() {
+  const container = document.getElementById("npc-container");
+  container.innerHTML = "";
+
+  npcs.forEach(npc => {
+    const div = document.createElement("div");
+    div.className = "npc";
+    div.innerHTML = `
+      <strong>${npc.name}</strong><br>
+      Hunger: ${npc.hunger}<br>
+      Energy: ${npc.energy}<br>
+      Mood: ${npc.mood}<br>
+      Action: ${npc.action}
+    `;
+    container.appendChild(div);
+  });
+}
+
+setInterval(() => {
+  npcs.forEach(npc => npc.tick());
+  render();
+}, 1000);
