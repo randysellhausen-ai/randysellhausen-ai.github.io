adbbba3943f9b7ee78c80baeb8491c7513e61fe7
randysellhausen-ai <randy.sellhausen@gmail.com>
2026-04-28 04:10:20 -0500

    Update npc-life-loop.js

--- npc-demo/npc-life-loop.js
+++ npc-demo/npc-life-loop.js
@@ -38,18 +38,40 @@ function render() {
   npcs.forEach(npc => {
     const div = document.createElement("div");
     div.className = "npc";
+
+    if (npc.action === "Wandering") div.classList.add("wandering");
+    if (npc.action === "Eating") div.classList.add("eating");
+    if (npc.action === "Resting") div.classList.add("resting");
+
     div.innerHTML = `
       <strong>${npc.name}</strong><br>
-      Hunger: ${npc.hunger}<br>
-      Energy: ${npc.energy}<br>
-      Mood: ${npc.mood}<br>
-      Action: ${npc.action}
+      Action: ${npc.action}<br>
+
+      <div class="bar">
+        <div class="bar-fill hunger-fill" style="width:${npc.hunger}%;"></div>
+      </div>
+
+      <div class="bar">
+        <div class="bar-fill energy-fill" style="width:${npc.energy}%;"></div>
+      </div>
+
+      <div class="bar">
+        <div class="bar-fill mood-fill" style="width:${npc.mood}%;"></div>
+      </div>
     `;
+
     container.appendChild(div);
   });
+}
+
+function tickPulse() {
+  const pulse = document.getElementById("tick-pulse");
+  pulse.style.opacity = 1;
+  setTimeout(() => pulse.style.opacity = 0, 200);
 }
 
 setInterval(() => {
   npcs.forEach(npc => npc.tick());
   render();
+  tickPulse();
 }, 1000);
